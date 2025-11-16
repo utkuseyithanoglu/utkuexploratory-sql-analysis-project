4904b4be7d99f3b140f8a7dfdab4573224977110
utkuseyithanoglu <utkuseyithanoglu@hotmail.com>
2025-11-16 22:31:56 +0000

    last 5

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -16,10 +16,13 @@
 -- -----------------------------------------------
 
 --SELECT * FROM regions;
+-- regions    id             name    country
 --SELECT * FROM species;
+-- species / id       scientific_name               common_name       genus        family      order_name
 --SELECT * FROM climate;
+-- id, region_id, avg_temperature, precipitation
 --SELECT * FROM observations;
-
+-- id  species_id  region_id     observer observation_date   latitude  longitude  count
 
 -- MISSION 1
 -- Your query here;
@@ -71,9 +74,31 @@ LIMIT 1;
 
 -- MISSION 7
 -- Your query here;
-SELECT *
-FROM regions
-LIMIT 3;
+--What are the 5 most frequent species_id?
+--Group, order by descending count, and limit the result
+SELECT species.common_name,species.id,COUNT(observations.id) AS "frequent"
+FROM species
+JOIN observations
+ON species.id =observations.species_id
+Group BY (species.common_name)
+ORDER BY frequent DESC
+LIMIT 5;
 
 -- MISSION 8
+--Which species (species_id) have fewer than 5 records?
+--Group by species and use HAVING to apply a condition.
 -- Your query here;
+SELECT species.id,species.common_name,COUNT(observations.species_id) AS "counts"
+FROM species
+JOIN observations
+ON species.id =observations.species_id
+Group BY (species.id)
+HAVING counts <= 5;
+-- Which observers (observer) recorded the most observations?
+--Group by observer name and count the records.
+SELECT observations.observer,observations.count
+FROM observations
+GROUP BY (observations.count)
+ORDER BY  observations.count DESC
+LIMIT 1;
+--
